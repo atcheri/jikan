90684e6d9fe3be1be76d6319c640f7bbb4076ac5
Atcheri Atch <atcheri@gmail.com>
2023-01-09 11:11:51 +0100

    feat: add reactive zoned date in time store

--- src/lib/Clock.svelte
+++ src/lib/Clock.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { time } from "../store/time";
+  import { time, zonedDate } from "../store/time";
 
   const formatter = new Intl.DateTimeFormat("en", {
     hour12: false,
@@ -7,11 +7,19 @@
     minute: "2-digit",
     second: "2-digit",
   });
+
+  $: hours = $zonedDate.getHours();
+  $: minutes = $zonedDate.getMinutes();
+  $: seconds = $zonedDate.getSeconds();
 </script>
 
 <div>
   Clock show time!
   <h1>The time is {formatter.format($time)}</h1>
+  <div>{hours}</div>
+  <div>{minutes}</div>
+  <div>{seconds}</div>
+  <div />
 </div>
 
 <style>
--- src/store/time.ts
+++ src/store/time.ts
@@ -1,7 +1,18 @@
-import { derived, readable, writable } from "svelte/store";
+import { derived, readable, writable, type Readable } from "svelte/store";
+import { utcToZonedTime } from "date-fns-tz";
+
+export const zones = [
+  "Europe/Paris",
+  "Asia/Tokyo",
+  "Asia/Kolkata",
+  "Asia/Shanghai",
+  "America/New_York",
+];
+
+const zone = readable(zones[0]);
 
 // export const time = readable(new Date(), function start(set) {
-export const time = writable(new Date(), function start(set) {
+export const time = readable(new Date(), function start(set) {
   const interval = setInterval(() => {
     set(new Date());
   }, 1000);
@@ -10,3 +21,11 @@ export const time = writable(new Date(), function start(set) {
     clearInterval(interval);
   };
 });
+
+export const zonedDate = derived<[Readable<Date>, Readable<string>], Date>(
+  [time, zone],
+  ([$time, $zone]) => {
+    const zonedDate = utcToZonedTime($time, $zone);
+    return zonedDate;
+  }
+);
