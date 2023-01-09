3be2d5c5ddaaf85826c25bfa95ca238dd184522a
Atcheri Atch <atcheri@gmail.com>
2023-01-09 10:29:48 +0100

    feat: center content and add main colors

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+  import Clock from "./lib/Clock.svelte";
 </script>
 
-<main>
+<main
+  class="flex flex-col justify-center items-center bg-gray-900 h-screen text-slate-100"
+>
   <h1>Vite + Svelte + TS + Tailwindcss</h1>
   <h2>Starter kit</h2>
+  <Clock />
 </main>
 
 <style>
